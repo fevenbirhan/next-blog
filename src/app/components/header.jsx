b3c6b51770d8af138570843c0c986f90708cb8ce
fevenbirhan <149194091+fevenbirhan@users.noreply.github.com>
2025-03-08 01:55:22 +0300

    create first template

---
+++ src/app/components/header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Button } from 'flowbite-react'
+
+export default function Header() {
+  return (
+    <Button gradientDuoTone='purpleToBlue'>Header</Button>
+  )
+}
