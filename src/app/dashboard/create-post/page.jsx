06e61026bd210caeb517535948da0d97728203cb
fevenbirhan <149194091+fevenbirhan@users.noreply.github.com>
2025-03-14 02:17:14 +0300

    Upload post images using supabase

---
+++ src/app/dashboard/create-post/page.jsx
@@ -0,0 +1,137 @@
+'use client';
+ 
+ import { useUser } from '@clerk/nextjs';
+ import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react';
+ import { useState } from 'react';
+ 
+ import dynamic from 'next/dynamic';
+ const ReactQuill = dynamic(() => import('react-quill-new'), { ssr: false });
+ // https://dev.to/a7u/reactquill-with-nextjs-478b
+ import 'react-quill-new/dist/quill.snow.css';
+
+ import { supabase } from '@/supabase';
+ import { CircularProgressbar } from 'react-circular-progressbar';
+ import 'react-circular-progressbar/dist/styles.css';
+ 
+ export default function CreatePostPage() {
+   const { isSignedIn, user, isLoaded } = useUser();
+   const [file, setFile] = useState(null);
+   const [imageUploadProgress, setImageUploadProgress] = useState(null);
+   const [imageUploadError, setImageUploadError] = useState(null);
+   const [formData, setFormData] = useState({});
+
+   const handleUploadImage = async () => {
+        try {
+        if (!file) {
+            setImageUploadError('Please select an image');
+            return;
+        }
+        setImageUploadError(null);
+        
+        const fileName = `${new Date().getTime()}-${file.name}`;
+        setImageUploadProgress(0);
+        
+        const { data, error } = await supabase.storage.from('next-blog').upload(fileName, file, {
+            cacheControl: '3600',
+            upsert: false,
+        });
+        
+        if (error) {
+            throw error;
+        }
+        
+        const { data: urlData } = await supabase.storage.from('next-blog').getPublicUrl(fileName);
+        setImageUploadProgress(null);
+        setImageUploadError(null);
+        setFormData({ ...formData, image: urlData.publicUrl });
+        } catch (error) {
+        setImageUploadError('Image upload failed');
+        setImageUploadProgress(null);
+        console.error(error);
+        }
+   };
+ 
+   if (!isLoaded) {
+     return null;
+   }
+ 
+   if (isSignedIn && user.publicMetadata.isAdmin) {
+     return (
+       <div className='p-3 max-w-3xl mx-auto min-h-screen'>
+         <h1 className='text-center text-3xl my-7 font-semibold'>
+           Create a post
+         </h1>
+         <form className='flex flex-col gap-4'>
+           <div className='flex flex-col gap-4 sm:flex-row justify-between'>
+             <TextInput
+               type='text'
+               placeholder='Title'
+               required
+               id='title'
+               className='flex-1'
+             />
+             <Select>
+               <option value='uncategorized'>Select a category</option>
+               <option value='javascript'>JavaScript</option>
+               <option value='reactjs'>React.js</option>
+               <option value='nextjs'>Next.js</option>
+             </Select>
+           </div>
+           <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
+             <FileInput 
+                type='file' 
+                accept='image/*' 
+                onChange={(e) => setFile(e.target.files[0])}
+             />
+             <Button
+               type='button'
+               gradientDuoTone='purpleToBlue'
+               size='sm'
+               outline
+               onClick={handleUploadImage}
+               disabled={imageUploadProgress}
+             >
+                {imageUploadProgress ? (
+                 <div className='w-16 h-16'>
+                   <CircularProgressbar
+                     value={imageUploadProgress}
+                     text={`${imageUploadProgress || 0}%`}
+                   />
+                 </div>
+                ) : (
+                 'Upload Image'
+                )}
+             </Button>
+           </div>
+
+            {imageUploadError && (
+             <Alert color='failure'>{imageUploadError}</Alert>
+            )}
+            {formData.image && (
+             <img
+               src={formData.image}
+               alt='upload'
+               className='w-full h-72 object-cover'
+             />
+            )}
+ 
+           <ReactQuill
+             theme='snow'
+             placeholder='Write something...'
+             className='h-72 mb-12'
+             required
+           />
+           <Button type='submit' gradientDuoTone='purpleToPink'>
+             Publish
+           </Button>
+         </form>
+       </div>
+     );
+   } else {
+     return (
+       <h1 className='text-center text-3xl my-7 font-semibold'>
+         You are not authorized to view this page
+       </h1>
+     );
+   }
+ }
