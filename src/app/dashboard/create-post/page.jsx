29272caced47393c98297d0742f0effaa6533b14
fevenbirhan <149194091+fevenbirhan@users.noreply.github.com>
2025-03-14 02:36:28 +0300

    add posts to the database

--- src/app/dashboard/create-post/page.jsx
+++ src/app/dashboard/create-post/page.jsx
@@ -3,6 +3,7 @@
  import { useUser } from '@clerk/nextjs';
  import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react';
  import { useState } from 'react';
+ import { useRouter } from 'next/navigation';
  
  import dynamic from 'next/dynamic';
  const ReactQuill = dynamic(() => import('react-quill-new'), { ssr: false });
@@ -19,8 +20,11 @@
    const [imageUploadProgress, setImageUploadProgress] = useState(null);
    const [imageUploadError, setImageUploadError] = useState(null);
    const [formData, setFormData] = useState({});
+   const [publishError, setPublishError] = useState(null);
+   const router = useRouter();
+   console.log(formData);
 
-   const handleUploadImage = async () => {
+    const handleUploadImage = async () => {
         try {
         if (!file) {
             setImageUploadError('Please select an image');
@@ -49,19 +53,46 @@
         setImageUploadProgress(null);
         console.error(error);
         }
-   };
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          const res = await fetch('/api/post/create', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              ...formData,
+              userMongoId: user.publicMetadata.userMongoId,
+            }),
+          });
+          const data = await res.json();
+          if (!res.ok) {
+            setPublishError(data.message);
+            return;
+          }
+          if (res.ok) {
+            setPublishError(null);
+            router.push(`/post/${data.slug}`);
+          }
+        } catch (error) {
+          setPublishError('Something went wrong');
+        }
+    };
  
-   if (!isLoaded) {
+    if (!isLoaded) {
      return null;
-   }
+    }
  
-   if (isSignedIn && user.publicMetadata.isAdmin) {
+    if (isSignedIn && user.publicMetadata.isAdmin) {
      return (
        <div className='p-3 max-w-3xl mx-auto min-h-screen'>
          <h1 className='text-center text-3xl my-7 font-semibold'>
            Create a post
          </h1>
-         <form className='flex flex-col gap-4'>
+         <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
            <div className='flex flex-col gap-4 sm:flex-row justify-between'>
              <TextInput
                type='text'
@@ -69,8 +100,15 @@
                required
                id='title'
                className='flex-1'
+               onChange={(e) =>
+                setFormData({ ...formData, title: e.target.value })
+               }
              />
-             <Select>
+             <Select
+                onChange={(e) =>
+                 setFormData({ ...formData, category: e.target.value })
+                }
+             >
                <option value='uncategorized'>Select a category</option>
                <option value='javascript'>JavaScript</option>
                <option value='reactjs'>React.js</option>
@@ -120,6 +158,9 @@
              placeholder='Write something...'
              className='h-72 mb-12'
              required
+             onChange={(value) => {
+               setFormData({ ...formData, content: value });
+             }}
            />
            <Button type='submit' gradientDuoTone='purpleToPink'>
              Publish
@@ -127,11 +168,11 @@
          </form>
        </div>
      );
-   } else {
+    } else {
      return (
        <h1 className='text-center text-3xl my-7 font-semibold'>
          You are not authorized to view this page
        </h1>
      );
-   }
+    }
  }
